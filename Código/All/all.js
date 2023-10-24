4160e6d91e42fa746b4f97694e8eeb4eff7aefb5
Pedro H Moreira <pedrohmoreira.office@gmail.com>
2023-10-24 17:47:28 -0300

    Editando Js

--- Código/All/all.js
+++ Código/All/all.js
@@ -1,47 +1,8 @@
 
 const botao = document.getElementById("meuBotao");
 botao.addEventListener("click", function () {
-    alert("Entre em contato pelo nosso WhatsApp, temos desconto de 5%!");
+    alert("Para mais informações, Entre em contato pelo nosso WhatsApp, !");
 });
 
 
 
-// let fruits;
-
-// fetch('https://www.fruityvice.com/')
-//     .then(response => response.json())
-//     .then(data => exibir(data))
-
-// function exibir(dados) {
-//     fruits = dados
-//     console.log(fruits)
-// }
-
-
-
-// Atividade CEP
-let buscarCep = document.getElementById("buscarCep");
-
-buscarCep.onclick = () => {
-    let cep = document.getElementById("cep").value;
-
-    if (cep.length != 8) {
-        alert("O CEP precisa ter 8 digitos.")
-    } else {
-        fetch(`https://viacep.com.br/ws/${cep}/json/`)
-            .then(response => response.json())
-            .then(response => exibirInformacoesNaTela(response))
-    }
-}
-
-function exibirInformacoesNaTela(informacao) {
-    let rua = document.getElementById("rua");
-    let bairro = document.getElementById("bairro");
-    let cidade = document.getElementById("cidade");
-    let estado = document.getElementById("estado");
-
-    rua.value = informacao.logradouro;
-    bairro.value = informacao.bairro; ( peso , quantidade, categoria , valor)
-    cidade.value = informacao.localidade;
-    estado.value = informacao.uf;
-}
