7a545a333996dea4125de900f01e2f8ed189224e
Pedro H Moreira <pedrohmoreira.office@gmail.com>
2023-10-24 19:19:23 -0300

    JS e Css Editado

--- Código/All/all.js
+++ Código/All/all.js
@@ -1,8 +1,40 @@
 
-const botao = document.getElementById("meuBotao");
+const botao = document.getElementById("alerta-botao");
 botao.addEventListener("click", function () {
     alert("Para mais informações, Entre em contato pelo nosso WhatsApp, !");
 });
 
 
+let inputSuco = document.getElementById("pesquisa");
+let btnSuco = document.getElementById("btn-Suco");
 
+btnSuco.onclick = () => {
+    if (inputSuco.value == "laranja")
+        document.getElementById("suco-Maca").style.display = "none";
+    document.getElementById("suco-Morango").style.display = "none";
+    document.getElementById("suco-Abacaxi").style.display = "none";
+
+    document.getElementById("suco-Laranja").style.display = "block";
+
+    if (inputSuco.value == "morango")
+        document.getElementById("suco-Maca").style.display = "none";
+    document.getElementById("suco-Abacaxi").style.display = "none";
+    document.getElementById("suco-Laranja").style.display = "none";
+
+    document.getElementById("suco-Morango").style.display = "block";
+
+    if (inputSuco.value == "maca")
+        document.getElementById("suco-Abacaxi").style.display = "none";
+    document.getElementById("suco-Morango").style.display = "none";
+    document.getElementById("suco-Laranja").style.display = "none";
+
+    document.getElementById("suco-Maca").style.display = "block";
+
+    if (inputSuco.value == "abacaxi")
+        document.getElementById("suco-Maca").style.display = "none";
+    document.getElementById("suco-Morango").style.display = "none";
+    document.getElementById("suco-Laranja").style.display = "none";
+    
+    document.getElementById("suco-Abacaxi").style.display = "block";
+
+}
