ae6f88b5198f635d64d45c5702861d91393343a8
Pedro H Moreira <pedrohmoreira.office@gmail.com>
2023-11-13 15:00:56 -0300

    Editado Js e Evento

--- Código/About/script.js
+++ Código/About/script.js
@@ -1,6 +1,6 @@
 const botao = document.getElementById("alerta-botao");
 botao.addEventListener("click", function () {
-    alert("Entre em contato pelo nosso WhatsApp, temos desconto de 5%!");
+    alert("Entre em contato pelo nosso WhatsApp,");
 });
 
 window.addEventListener('scroll', function() {
